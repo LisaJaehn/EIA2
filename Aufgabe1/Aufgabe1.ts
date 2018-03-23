7308e648540187e5f932b06a0c76e05a2ab57264
lisaj <lisaj@DESKTOP-V4ODUCI.hfu-open.fu.hs-furtwangen.de>
2018-03-23 14:14:39 +0100

    aufgabe1

--- Aufgabe1/Aufgabe1.ts
+++ Aufgabe1/Aufgabe1.ts
@@ -3,7 +3,7 @@ function Hallo() {
   var name = prompt("Wie heißt du?", "");
   if (name != null) {
       
-    document.getElementById("Type").innerHTML =
+    document.getElementById("Name").innerHTML =
     
       "Servus " + name + "! Schöner Name!";
   }
