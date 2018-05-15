a5f20f40a943db5997d620637e9df4e8d9878337
lisaj <lisaj@10.15.10.230>
2018-05-15 15:10:59 +0200

    Aufgabe6

---
+++ Aufgabe6/Aufgabe6.2.js
@@ -0,0 +1,83 @@
+var L06_Interfaces;
+(function (L06_Interfaces) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Init");
+        //Enventlistener auf Button übergeben
+        let insertButton = document.getElementById("insert");
+        let searchButton = document.getElementById("search");
+        let refreshButton = document.getElementById("refresh");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+        searchButton.addEventListener("click", search);
+    }
+    //Funktion um Daten der Studenten zu speichern
+    function insert(_event) {
+        let inputs = document.getElementsByTagName("input");
+        let genderButton = document.getElementById("male");
+        let matrikel = inputs[2].value;
+        let studi;
+        //Interface übergeben
+        studi = {
+            name: inputs[0].value,
+            firstname: inputs[1].value,
+            matrikel: parseInt(matrikel),
+            age: parseInt(inputs[3].value),
+            gender: genderButton.checked,
+            studiengang: document.getElementsByTagName("select").item(0).value,
+        };
+        console.log(studi);
+        console.log(studi.age);
+        console.log(studi["age"]);
+        // Datensatz im assoziativen Array unter der Matrikelnummer speichern
+        L06_Interfaces.studiHomoAssoc[matrikel] = studi;
+        // nur um das auch noch zu zeigen...
+        L06_Interfaces.studiSimpleArray.push(studi);
+    }
+    //Funktion Ausgabe der Information
+    function refresh(_event) {
+        let output = document.getElementsByTagName("textarea")[1];
+        output.value = "";
+        // for-in-Schleife iteriert über die Schlüssel des assoziativen Arrays
+        for (let matrikel in L06_Interfaces.studiHomoAssoc) {
+            let studi = L06_Interfaces.studiHomoAssoc[matrikel];
+            let line = matrikel + ": ";
+            line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            line += studi.studiengang + ": ";
+            output.value += line + "\n";
+        }
+        // zusätzliche Konsolenausgaben zur Demonstration
+        console.group("Simple Array");
+        console.log(L06_Interfaces.studiSimpleArray);
+        console.groupEnd();
+        console.group("Associatives Array (Object)");
+        console.log(L06_Interfaces.studiHomoAssoc);
+        console.groupEnd();
+    }
+    //Funktion, um Studenten nach Matrikelnummer zu suchen
+    //Funktion search aufstellen
+    function search(_event) {
+        //Auf erste Textarea zugreifen
+        let output = document.getElementsByTagName("textarea")[0];
+        output.value = "";
+        //Zugriff auf Inputs
+        let inputs = document.getElementsByTagName("input");
+        //Matrikel wird aufgerufen durch den 6. Input
+        let matrikel = inputs[6].value;
+        //Matrikelnummer wird gespeichert
+        let studi = L06_Interfaces.studiHomoAssoc[matrikel];
+        if (studi) {
+            //Übereinstimmung mit Student
+            let line = matrikel + ": ";
+            line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? ", (M)" : ", (F)";
+            line += studi.studiengang + ": ";
+            output.value += line + "\n";
+        }
+        else {
+            alert("Es wurde kein Student gefunden, bitte versuchen sie es noch einmal.");
+        }
+    }
+})(L06_Interfaces || (L06_Interfaces = {}));
+//# sourceMappingURL=Aufgabe6.2.js.map
---
+++ Aufgabe6/Aufgabe6.2.ts
@@ -0,0 +1,122 @@
+namespace L06_Interfaces {
+    window.addEventListener( "load", init );
+
+    function init( _event: Event ): void {
+        console.log( "Init" );
+
+        //Enventlistener auf Button übergeben
+
+        let insertButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById( "insert" );
+        let searchButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById( "search" );
+        let refreshButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById( "refresh" );
+        insertButton.addEventListener( "click", insert );
+        refreshButton.addEventListener( "click", refresh );
+        searchButton.addEventListener( "click", search );
+    }
+
+
+    //Funktion um Daten der Studenten zu speichern
+
+    function insert( _event: Event ): void {
+        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName( "input" );
+        let genderButton: HTMLInputElement = <HTMLInputElement>document.getElementById( "male" );
+        let matrikel: string = inputs[2].value;
+        let studi: Studi;
+
+        //Interface übergeben
+
+        studi = {
+            name: inputs[0].value,
+            firstname: inputs[1].value,
+            matrikel: parseInt( matrikel ),
+            age: parseInt( inputs[3].value ),
+            gender: genderButton.checked,
+            studiengang: document.getElementsByTagName( "select" ).item( 0 ).value,
+        };
+
+
+
+        console.log( studi );
+        console.log( studi.age );
+        
+        
+        
+        console.log( studi["age"] );
+
+        // Datensatz im assoziativen Array unter der Matrikelnummer speichern
+        studiHomoAssoc[matrikel] = studi;
+
+
+        // nur um das auch noch zu zeigen...
+        studiSimpleArray.push( studi );
+    }
+
+
+
+    //Funktion Ausgabe der Information
+
+    function refresh( _event: Event ): void {
+        let output: HTMLTextAreaElement = document.getElementsByTagName( "textarea" )[1];
+        output.value = "";
+        // for-in-Schleife iteriert über die Schlüssel des assoziativen Arrays
+
+        for ( let matrikel in studiHomoAssoc ) {  // Besonderheit: Type-Annotation nicht erlaubt, ergibt sich aus der Interface-Definition
+            let studi: Studi = studiHomoAssoc[matrikel];
+            let line: string = matrikel + ": ";
+            line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            line += studi.studiengang + ": ";
+            output.value += line + "\n";
+        }
+
+        // zusätzliche Konsolenausgaben zur Demonstration
+        console.group( "Simple Array" );
+        console.log( studiSimpleArray );
+        console.groupEnd();
+
+        console.group( "Associatives Array (Object)" );
+        console.log( studiHomoAssoc );
+        console.groupEnd();
+    }
+
+    //Funktion, um Studenten nach Matrikelnummer zu suchen
+
+    //Funktion search aufstellen
+
+    function search( _event: Event ): void {
+
+        //Auf erste Textarea zugreifen
+
+        let output: HTMLTextAreaElement = document.getElementsByTagName( "textarea" )[0];
+
+        output.value = "";
+
+        //Zugriff auf Inputs
+
+        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName( "input" );
+
+        //Matrikel wird aufgerufen durch den 6. Input
+
+        let matrikel: string = inputs[6].value;
+
+        //Matrikelnummer wird gespeichert
+
+        let studi: Studi = studiHomoAssoc[matrikel];
+
+        if ( studi ) {
+            
+            //Übereinstimmung mit Student
+
+            let line: string = matrikel + ": ";
+            line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? ", (M)" : ", (F)";
+            line += studi.studiengang + ": ";
+            output.value += line + "\n";
+            
+        //Keine Übereinstimmung mit Student
+
+        } else {
+            alert( "Es wurde kein Student gefunden, bitte versuchen sie es noch einmal." );
+        }
+    }
+}
