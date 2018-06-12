7157155d95b35b8153bb17910599ca03ba854c17
lisaj <lisajaehn1@web.de>
2018-06-12 13:19:22 +0200

    Canvas

---
+++ Augabe8/Canvas.js
@@ -0,0 +1,11 @@
+var Canvas_09;
+(function (Canvas_09) {
+    window.addEventListener("load", init);
+    let crc2;
+    function init(_event) {
+        let canvas = document.getElementsByTagName("canvas")[0];
+        crc2 = canvas.getContext("2d");
+        console.log(crc2);
+    }
+})(Canvas_09 || (Canvas_09 = {}));
+//# sourceMappingURL=Canvas.js.map
---
+++ Augabe8/Canvas.ts
@@ -0,0 +1,64 @@
+namespace Canvas_09 {
+
+    window.addEventListener( "load", init );
+    let crc2: CanvasRenderingContext2D;
+
+    function init( _event: Event ): void {
+        let canvas: HTMLCanvasElement = document.getElementsByTagName( "canvas" )[0];
+        crc2 = canvas.getContext( "2d" );
+        console.log( crc2 );
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
