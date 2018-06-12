13a2957b71fe73dac3a1592eb1999d3368dc959f
lisaj <lisajaehn1@web.de>
2018-06-12 14:03:57 +0200

    Canvas

--- Augabe8/Canvas.js
+++ Augabe8/Canvas.js
@@ -6,6 +6,12 @@ var L08_Canvas;
         let canvas = document.getElementsByTagName("canvas")[0];
         crc2 = canvas.getContext("2d");
         console.log(crc2);
+        //Boden
+        crc2.fillStyle = "rgb(189,183,107)";
+        crc2.fillRect(0, 0, 800, 600);
+        //Wasser
+        crc2.fillStyle = "rgb(100,149, 237)";
+        crc2.fillRect(0, 0, 800, 400);
     }
 })(L08_Canvas || (L08_Canvas = {}));
 //# sourceMappingURL=Canvas.js.map
--- Augabe8/Canvas.ts
+++ Augabe8/Canvas.ts
@@ -8,12 +8,22 @@ namespace L08_Canvas {
         let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
         crc2 = canvas.getContext("2d");
         console.log(crc2);
-
         
+        //Boden
+        
+        crc2.fillStyle = "rgb(189,183,107)";
+        crc2.fillRect(0, 0, 800, 600);
+        
+        //Wasser
+        
+        crc2.fillStyle = "rgb(100,149, 237)";
+        crc2.fillRect(0, 0, 800, 400);
+
     }
 
-   
-    }
+    
+
+}
 
     
     
