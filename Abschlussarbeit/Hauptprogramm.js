811abe03ddfa54babd344e9f23279514acd1169b
lisaj <lisajaehn1@web.de>
2018-07-07 14:59:00 +0200

    Abschlussarbeit

--- Abschlussarbeit/Hauptprogramm.js
+++ Abschlussarbeit/Hauptprogramm.js
@@ -8,24 +8,22 @@ var Abschlussarbeit;
         console.log(crc2);
         //Hintergrund
         drawBackground(100);
-        //Äpfel
+        //Rote Kugeln
         for (let i = 0; i < 8; i++) {
             let x = Math.random() * crc2.canvas.width;
             let y = Math.random() * crc2.canvas.height;
-            drawApples(x, y, 10);
+            drawBubbleRed(x, y, 10);
         }
-        //Orangen
+        //Orangene Kugeln
         for (let i = 0; i < 8; i++) {
             let x = Math.random() * crc2.canvas.width;
             let y = Math.random() * crc2.canvas.height;
-            drawOranges(x, y, 10);
+            drawBubbleOrange(x, y, 10);
         }
         //Korb
         drawBasket(450, 630);
-        //Bäume
-        drawTrees(100, 600);
-        //Wolken
-        drawClouds(200, 200);
+        //Baum
+        drawTree(150, 550);
     }
     //Funktion Hintergrund
     function drawBackground(_forestHeight) {
@@ -35,7 +33,7 @@ var Abschlussarbeit;
         crc2.fillRect(0, crc2.canvas.height - _forestHeight, crc2.canvas.width, crc2.canvas.height);
     }
     //Äpfel
-    function drawApples(_x, _y, _radius) {
+    function drawBubbleRed(_x, _y, _radius) {
         crc2.beginPath();
         crc2.fillStyle = "rgb(255,0,0)";
         crc2.arc(_x, _y, _radius, 0, 2 * Math.PI);
@@ -44,7 +42,7 @@ var Abschlussarbeit;
         crc2.fill();
     }
     //Orangen
-    function drawOranges(_x, _y, _radius) {
+    function drawBubbleOrange(_x, _y, _radius) {
         crc2.beginPath();
         crc2.fillStyle = "rgb(255,144,0)";
         crc2.arc(_x, _y, _radius, 0, 2 * Math.PI);
@@ -72,66 +70,37 @@ var Abschlussarbeit;
         crc2.stroke();
         //crc2.fill();
     }
-    //Bäume
-    function drawTrees(_x, _y) {
+    //Tannenbaum
+    function drawTree(_x, _y) {
+        crc2.beginPath();
+        crc2.fillStyle = "rgb(0,100,0)";
+        crc2.moveTo(150, 550);
+        crc2.lineTo(50, 550);
+        crc2.lineTo(150, 450);
+        crc2.lineTo(70, 450);
+        crc2.lineTo(150, 350);
+        crc2.lineTo(90, 350);
+        crc2.lineTo(175, 250);
+        crc2.lineTo(260, 350);
+        crc2.lineTo(200, 350);
+        crc2.lineTo(280, 450);
+        crc2.lineTo(200, 450);
+        crc2.lineTo(300, 550);
+        crc2.lineTo(200, 550);
+        //crc2.closePath();
+        crc2.stroke();
+        crc2.fill();
+        //Baumstamm
+        drawTrunk(150, 600);
+    }
+    //Tannenbaumstamm
+    function drawTrunk(_x, _y) {
         crc2.beginPath();
         crc2.fillStyle = "rgb(139,90,0)";
-        crc2.moveTo(100, 600);
-        crc2.bezierCurveTo(150, 480, 150, 300, 150, 300);
-        crc2.moveTo(150, 300);
-        crc2.lineTo(250, 300);
-        crc2.moveTo(250, 300);
-        crc2.bezierCurveTo(250, 300, 250, 480, 300, 600);
-        crc2.moveTo(300, 600);
-        crc2.lineTo(100, 600);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fill();
-        //Laubhaufen
-        drawBush2(180, 220, 70);
-        drawBush1(120, 280, 70);
-        drawBush3(180, 300, 70);
-        drawBush4(240, 260, 70);
-    }
-    function drawBush1(_x, _y, _radius) {
-        crc2.beginPath();
-        crc2.fillStyle = "rgb(0,139,0)";
-        crc2.arc(_x, _y, _radius, 0, 2 * Math.PI);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fill();
-    }
-    function drawBush3(_x, _y, _radius) {
-        crc2.beginPath();
-        crc2.fillStyle = "rgb(188,238,104)";
-        crc2.arc(_x, _y, _radius, 0, 2 * Math.PI);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fill();
-    }
-    function drawBush2(_x, _y, _radius) {
-        crc2.beginPath();
-        crc2.fillStyle = "rgb(188,238,104)";
-        crc2.arc(_x, _y, _radius, 0, 2 * Math.PI);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fill();
-    }
-    function drawBush4(_x, _y, _radius) {
-        crc2.beginPath();
-        crc2.fillStyle = "rgb(0,139,0)";
-        crc2.arc(_x, _y, _radius, 0, 2 * Math.PI);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fill();
-    }
-    function drawClouds(_x, _y) {
-        crc2.beginPath();
-        crc2.fillStyle = "rgb(255,250,250)";
-        crc2.moveTo(200, 200);
-        crc2.bezierCurveTo(150, 160, 230, 120, 260, 160);
-        crc2.moveTo(260, 160);
-        crc2.bezierCurveTo(280, 150, 320, 140, 350, 170);
+        crc2.moveTo(150, 600);
+        crc2.lineTo(150, 550);
+        crc2.lineTo(200, 550);
+        crc2.lineTo(200, 600);
         crc2.closePath();
         crc2.stroke();
         crc2.fill();
